ae00eddf75baa4e32c99a57a257c589b5bb20089
Bryce Davis <bad1128@gmail.com>
2014-05-30 23:43:30 +0000

    Add zsh configuration to installation

--- install.sh
+++ install.sh
@@ -48,12 +48,14 @@ setup () {
 installConfigs() {
     TMUX=.tmux.conf
     VIM=.vimrc
+    ZSH=.zshrc
     UNINSTALL=uninstall.sh
 
     CONFIG_DIR=$(pwd)/tmp
     CONFIG_BAK_DIR=~/config-backup
     TMUX_DIR=$CONFIG_DIR/$TMUX
     VIM_DIR=$CONFIG_DIR/$VIM
+    ZSH_DIR=$CONFIG_DIR/$ZSH
     UNINSTALL_DIR=$CONFIG_DIR/$UNINSTALL
 
     if [ -d $CONFIG_DIR ]; then
@@ -74,10 +76,17 @@ installConfigs() {
         mv ~/$VIM $CONFIG_BAK_DIR
     fi
 
+    if [ -e ~/$ZSH ]; then
+        if [ ! -d $CONFIG_BAK_DIR ]; then
+            mkdir $CONFIG_BAK_DIR
+        fi
+        mv ~/$ZSH $CONFIG_BAK_DIR
+
     git clone https://github.com/mahimahi42/config-sync.git tmp
 
     cp $TMUX_DIR ~
     cp $VIM_DIR ~
+    cp $ZSH_DIR ~
     cp $UNINSTALL_DIR ~/.uninstall_configs
     chmod u+x ~/.uninstall_configs
 
