9c81cb97253b670eac52cfdf304d7c28f8a1d217
Bryce Davis <bad1128@gmail.com>
2014-05-30 13:54:38 -0400

    Update install script to download configs after prompt

--- install.sh
+++ install.sh
@@ -5,8 +5,6 @@ OS=$(uname -a)
 TMUX_INSTALL_DIR=~/.tmux.conf
 VIM_INSTALL_DIR=~/.vimrc
 
-git clone https://github.com/mahimahi42/config-sync.git
-
 TMUX_DIR=$(pwd)/config-sync/.tmux.conf
 VIM_DIR=$(pwd)/config-sync/.vimrc
 
@@ -14,6 +12,8 @@ read  -p "Begin installation? [y/n]: " RESP
 if [ ! "$RESP" = "y" ]; then
     exit
 fi
+
+git clone https://github.com/mahimahi42/config-sync.git
 
 ln -s ${TMUX_DIR} ${TMUX_INSTALL_DIR}
 ln -s ${VIM_DIR} ${VIM_INSTALL_DIR}
