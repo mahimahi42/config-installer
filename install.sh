e45dcd24a5d8a3b6eb2084794c6702c49a5ef938
Bryce Davis <bad1128@gmail.com>
2014-05-30 13:51:36 -0400

    Add finished config files for tmux and vim, update install script to prompt user before installation

--- install.sh
+++ install.sh
@@ -8,5 +8,10 @@ VIM_INSTALL_DIR=~/.vimrc
 TMUX_DIR=$(pwd)/.tmux.conf
 VIM_DIR=$(pwd)/.vimrc
 
+read  -p "Begin installation? [y/n]: " RESP
+if [ ! "$RESP" = "y" ]; then
+    exit
+fi
+
 ln -s ${TMUX_DIR} ${TMUX_INSTALL_DIR}
 ln -s ${VIM_DIR} ${VIM_INSTALL_DIR}
