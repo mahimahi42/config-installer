fd225eb6572af6232a065e2bd0a48fc998070dff
Bryce Davis <bad1128@gmail.com>
2014-06-02 09:31:02 -0400

    Fix merge problems in install script

--- install.sh
+++ install.sh
@@ -19,15 +19,13 @@ installGit () {
     esac
 }
 
-<<<<<<< HEAD
 TMUX_INSTALL_DIR=$HOME/.tmux.conf
 VIM_INSTALL_DIR=$HOME/.vimrc
-=======
+
 installGitMessage () {
     echo "Operating System: $1"
     echo "I might need your sudo password, in order to install git."
 }
->>>>>>> d51c324adde79f726766dc36458f56f2f140e681
 
 checkForHomebrew () {
     BREW=$(which brew)
@@ -57,7 +55,7 @@ installConfigs() {
     UNINSTALL=uninstall.sh
 
     CONFIG_DIR=$(pwd)/tmp
-    CONFIG_BAK_DIR=~/config-backup
+    CONFIG_BAK_DIR=$HOME/config-backup
     TMUX_DIR=$CONFIG_DIR/$TMUX
     VIM_DIR=$CONFIG_DIR/$VIM
     ZSH_DIR=$CONFIG_DIR/$ZSH
@@ -67,34 +65,34 @@ installConfigs() {
         rm -rf $CONFIG_DIR
     fi
 
-    if [ -e ~/$TMUX ]; then
+    if [ -e $HOME/$TMUX ]; then
         if [ ! -d $CONFIG_BAK_DIR ]; then
             mkdir $CONFIG_BAK_DIR
         fi
-        mv ~/$TMUX $CONFIG_BAK_DIR
+        mv $HOME/$TMUX $CONFIG_BAK_DIR
     fi
 
-    if [ -e ~/$VIM ]; then
+    if [ -e $HOME/$VIM ]; then
         if [ ! -d $CONFIG_BAK_DIR ]; then
             mkdir $CONFIG_BAK_DIR
         fi
-        mv ~/$VIM $CONFIG_BAK_DIR
+        mv $HOME/$VIM $CONFIG_BAK_DIR
     fi
 
-    if [ -e ~/$ZSH ]; then
+    if [ -e $HOME/$ZSH ]; then
         if [ ! -d $CONFIG_BAK_DIR ]; then
             mkdir $CONFIG_BAK_DIR
         fi
-        mv ~/$ZSH $CONFIG_BAK_DIR
+        mv $HOME/$ZSH $CONFIG_BAK_DIR
     fi
 
     git clone https://github.com/mahimahi42/config-sync.git tmp
 
-    cp $TMUX_DIR ~
-    cp $VIM_DIR ~
-    cp $ZSH_DIR ~
-    cp $UNINSTALL_DIR ~/.uninstall_configs
-    chmod u+x ~/.uninstall_configs
+    cp $TMUX_DIR $HOME
+    cp $VIM_DIR $HOME
+    cp $ZSH_DIR $HOME
+    cp $UNINSTALL_DIR $HOME/.uninstall_configs
+    chmod u+x $HOME/.uninstall_configs
 
     rm -rf $CONFIG_DIR
 }
@@ -103,10 +101,10 @@ displayFinalMessage () {
     echo "\n\n\n"
     echo "All done!"
     echo "Installed configurations for:"
-    echo "tmux: ${HOME}/$TMUX"
-    echo "vim: ${HOME}/$VIM"
-    echo "zsh: ${HOME}/$ZSH"
-    echo "To uninstall, just run ${HOME}/.uninstall_configs"
+    echo "tmux: $HOME/$TMUX"
+    echo "vim: $HOME/$VIM"
+    echo "zsh: $HOME/$ZSH"
+    echo "To uninstall, just run $HOME/.uninstall_configs"
 }
 
 setup
