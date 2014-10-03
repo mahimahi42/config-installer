f924112e633c889dcd4bf3575c93f595485d3f37
Bryce Davis <BAD1128@gmail.com>
2014-10-03 10:27:09 -0400

    Add colors to install script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# Exit immediately on encountering an non-zero exit code
+set -e
+
+# COLORS!!
+black='\033[0;30m'
+white='\033[0;37m'
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+magenta='\033[0;35m'
+cyan='\033[0;36m'
+
+# Color Reset
+alias Reset="tput sgr0"
+
+# Color Echo
+cecho () {
+    echo "${2}${1}"
+    Reset
+    return
+}
+
