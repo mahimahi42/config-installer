50aee01d21f39c4587296425c6bb004f74d32384
Bryce Davis <BAD1128@gmail.com>
2014-10-03 10:04:03 -0400

    Backup old install script

---
+++ install_old.sh
@@ -0,0 +1,112 @@
+#!/usr/bin/env sh
+
+displayWelcomeMessage () {
+    clear
+
+    echo "Welcome to my development environment setup script!"
+    echo "I'm going to need git, since the configuration files are hosted on GitHub."
+}
+
+checkForGit () {
+#    GIT_AVAIL=$(which git)
+#    if [ -z $GIT_AVAIL ]; then
+#        echo "Checking for git... Not found, will install"
+#        installGit
+#    else
+#        echo "Checking for git... $GIT_AVAIL"
+#    fi
+    if ! hash git 2>/dev/null; then
+        echo "Checking for git... Not found, will install"
+        installGit
+    else
+        echo "Checking for git... $(command -v git)"
+    fi
+}
+
+installGit () {
+    OS=$(cat /etc/*-release)
+    case "$OS" in
+        *Ubuntu*)
+            echo "I'm going to install git via apt. I may need your sudo password."
+            sudo apt-get -y install git
+            ;;
+        *Hat*)
+            echo "I'm going to install git via yum. I may need your sudo password."
+            sudo yum -y install git
+            ;;
+    esac
+}
+
+backupFile () {
+    if [ -e $HOME/$1 ]; then
+        if [ ! -d $2 ]; then
+            mkdir $2
+        fi
+        mv $HOME/$1 $2
+    fi
+}
+
+backupConfigs () {
+    BAK_DIR=$HOME/.config-backups/
+
+    backupFile .tmux.conf $BAK_DIR
+    backupFile .vimrc $BAK_DIR
+    backupFile .zshrc $BAK_DIR
+
+#    if [ -e $HOME/.tmux.conf ]; then
+#        if [ ! -d $BAK_DIR ]; then
+#            mkdir $BAK_DIR
+#        fi
+#        mv $HOME/.tmux.conf $BAK_DIR
+#    fi
+
+#    if [ -e $HOME/.vimrc ]; then
+#        if [ ! -d $BAK_DIR ]; then
+#            mkdir $BAK_DIR
+#        fi
+#        mv $HOME/.vimrc $BAK_DIR
+#    fi
+
+#    if [ -e $HOME/.zshrc ]; then
+#        if [ ! -d $BAK_DIR ]; then
+#            mkdir $BAK_DIR
+#        fi
+#        mv $HOME/.zshrc $BAK_DIR
+#    fi
+}
+
+installNewConfigs () {
+    git clone https://github.com/mahimahi42/config-sync.git tmp
+
+    cp tmp/.tmux.conf $HOME
+    cp tmp/.vimrc $HOME
+    cp tmp/.zshrc $HOME
+    cp tmp/uninstall.sh $HOME/.uninstall_configs
+    chmod +x $HOME/.uninstall_configs
+
+    rm -rf tmp/
+}
+
+displayFinalMessage () {
+    echo "\n\n\n"
+    echo "All done!"
+    echo "Installed configurations for:"
+    echo "tmux: $HOME/.tmux.conf"
+    echo "vim: $HOME/.vimrc"
+    echo "zsh: $HOME/.zshrc"
+    echo "To uninstall, run $HOME/.uninstall_configs"
+}
+
+main () {
+    displayWelcomeMessage
+    
+    checkForGit
+
+    backupConfigs
+
+    installNewConfigs
+
+    displayFinalMessage
+}
+
+main
